7c451be403d0aab91ab0f9d646c62356fc6b207b
sachinmaile <maile.sachin16@gmail.com>
2023-08-04 10:59:08 +0530

    Creating a store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {legacy_createStore as createStore} from 'redux'
+// import { configureStore } from '@reduxjs/toolkit'
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store=createStore(movies);
+console.log('store',store);
+console.log('STATE',store.getState());
+// console.log('STATE',store.get)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
